7ef9e5c49a5b6ccbc543c8b808bac96c3f20df68
jreitmei <johannes.reitmeier@emerson.com>
2025-07-10 10:09:28 +0200

    gr-doa: Fixed calibrate_lin_array Testbench

--- python/doa/qa_calibrate_lin_array.py
+++ python/doa/qa_calibrate_lin_array.py
@@ -7,33 +7,204 @@
 #
 
 from gnuradio import gr, gr_unittest
+from gnuradio import blocks
+import itertools
+import oct2py
+import numpy
+import os
+
 # from gnuradio import blocks
 try:
     from gnuradio.doa import calibrate_lin_array
 except ImportError:
-    import os
     import sys
     dirname, filename = os.path.split(os.path.abspath(__file__))
     sys.path.append(os.path.join(dirname, "bindings"))
     from gnuradio.doa import calibrate_lin_array
 
-class qa_calibrate_lin_array(gr_unittest.TestCase):
+class qa_calibrate_lin_array (gr_unittest.TestCase):
 
-    def setUp(self):
-        self.tb = gr.top_block()
+    def setUp (self):
+        self.tb = gr.top_block ()
 
-    def tearDown(self):
+    def tearDown (self):
         self.tb = None
 
-    def test_instance(self):
-        # FIXME: Test will fail until you pass sensible arguments to the constructor
-        instance = calibrate_lin_array()
+    def test_001_t (self):	       
+        # normalized_spacing
+        norm_spacing = 0.3
+        # pilot angle
+        pilot_doa = 30.0
+        # number of antenna array elements
+        num_ant_ele = 4
+        # length of each snapshot
+        len_ss = 2048
+        # overlap size of each snapshot
+        overlap_size = 512
+        # apply Forward-Backward Averaging?
+        FB = False
+        # simulate perturbation?
+        PERTURB = True
 
-    def test_001_descriptive_test_name(self):
+        # Generate auto-correlation vector from octave
+        oc = oct2py.Oct2Py()
+        qa_tests_path = os.path.join(os.path.dirname(__file__), 'qa_tests')
+        oc.addpath(qa_tests_path)
+        # Add the parent directory containing the class folder
+        oc.addpath(os.path.dirname(qa_tests_path))
+        result = oc.doa_testbench_create('music_test_input_gen', len_ss, overlap_size, num_ant_ele, FB, 'linear', num_ant_ele, norm_spacing, PERTURB, pilot_doa, nout=3)
+        S_x, S_x_uncalibrated, ant_pert_vec = result
+        S_x_uncalibrated = S_x_uncalibrated.flatten().tolist()
+        ant_pert_vec = list(itertools.chain.from_iterable(ant_pert_vec))
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.vec_source = blocks.vector_source_c(S_x_uncalibrated, False, num_ant_ele*num_ant_ele)
+        self.doa_calibrate_lin_array_0 = calibrate_lin_array(norm_spacing, num_ant_ele, pilot_doa)
+        self.vec_sink = blocks.vector_sink_c(num_ant_ele)        
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.tb.connect((self.vec_source, 0), (self.doa_calibrate_lin_array_0, 0))
+        self.tb.connect((self.doa_calibrate_lin_array_0, 0), (self.vec_sink, 0))  
+
         # set up fg
-        self.tb.run()
-        # check data
+        self.tb.run ()
 
+        # get data from sink
+        ant_pert_vec_est = self.vec_sink.data()
+        ant_pert_vec_est = numpy.asarray(ant_pert_vec_est, 'F')
+        # num of snapshots
+        n_ss = len(ant_pert_vec_est)//num_ant_ele
+
+        # check data
+        ant_ele_range = range(0, num_ant_ele)
+        expected_result = numpy.zeros(num_ant_ele-1)+1j*numpy.zeros(num_ant_ele-1)
+        for ii in range(0, n_ss):
+            # calibrated_antenna_resp will contain elements that are all equal
+            calibrated_antenna_resp = numpy.divide(ant_pert_vec, ant_pert_vec_est[ii*num_ant_ele+numpy.array(ant_ele_range)])
+            self.assertComplexTuplesAlmostEqual(expected_result, numpy.diff(calibrated_antenna_resp), 1)
+
+    def test_002_t (self):	       
+        # normalized_spacing
+        norm_spacing = 0.5
+        # pilot angle
+        pilot_doa = 60.0
+        # number of antenna array elements
+        num_ant_ele = 8
+        # num of snapshots
+        n_ss = 1000
+        # length of each snapshot
+        len_ss = 1024
+        # overlap size of each snapshot
+        overlap_size = 128
+        # apply Forward-Backward Averaging?
+        FB = True
+        # simulate perturbation?
+        PERTURB = True
+
+        # Generate auto-correlation vector from octave
+        oc = oct2py.Oct2Py()
+        qa_tests_path = os.path.join(os.path.dirname(__file__), 'qa_tests')
+        oc.addpath(qa_tests_path)
+        # Add the parent directory containing the class folder
+        oc.addpath(os.path.dirname(qa_tests_path))
+        result = oc.doa_testbench_create('music_test_input_gen', len_ss, overlap_size, num_ant_ele, FB, 'linear', num_ant_ele, norm_spacing, PERTURB, pilot_doa, nout=3)
+        S_x, S_x_uncalibrated, ant_pert_vec = result
+        S_x_uncalibrated = S_x_uncalibrated.flatten().tolist()
+        ant_pert_vec = list(itertools.chain.from_iterable(ant_pert_vec))
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.vec_source = blocks.vector_source_c(S_x_uncalibrated, False, num_ant_ele*num_ant_ele)
+        self.doa_calibrate_lin_array_0 = calibrate_lin_array(norm_spacing, num_ant_ele, pilot_doa)
+        self.vec_sink = blocks.vector_sink_c(num_ant_ele)        
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.tb.connect((self.vec_source, 0), (self.doa_calibrate_lin_array_0, 0))
+        self.tb.connect((self.doa_calibrate_lin_array_0, 0), (self.vec_sink, 0))  
+
+        # set up fg
+        self.tb.run ()
+
+        # get data from sink
+        ant_pert_vec_est = self.vec_sink.data()
+        ant_pert_vec_est = numpy.asarray(ant_pert_vec_est, 'F')
+        # num of snapshots
+        n_ss = len(ant_pert_vec_est)//num_ant_ele
+
+        # check data
+        ant_ele_range = range(0, num_ant_ele)
+        expected_result = numpy.zeros(num_ant_ele-1)+1j*numpy.zeros(num_ant_ele-1)
+        for ii in range(0, n_ss):
+            # calibrated_antenna_resp will contain elements that are all equal
+            calibrated_antenna_resp = numpy.divide(ant_pert_vec, ant_pert_vec_est[ii*num_ant_ele+numpy.array(ant_ele_range)])
+            self.assertComplexTuplesAlmostEqual(expected_result, numpy.diff(calibrated_antenna_resp), 1)
+
+    def test_003_t (self):	       
+        # normalized_spacing
+        norm_spacing = 0.2
+        # pilot angle
+        pilot_doa = 25.0
+        # number of antenna array elements
+        num_ant_ele = 4
+        # num of snapshots
+        n_ss = 800
+        # length of each snapshot
+        len_ss = 256
+        # overlap size of each snapshot
+        overlap_size = 64
+        # apply Forward-Backward Averaging?
+        FB = False
+        # simulate perturbation?
+        PERTURB = True
+
+        # Generate auto-correlation vector from octave
+        oc = oct2py.Oct2Py()
+        qa_tests_path = os.path.join(os.path.dirname(__file__), 'qa_tests')
+        oc.addpath(qa_tests_path)
+        # Add the parent directory containing the class folder
+        oc.addpath(os.path.dirname(qa_tests_path))
+        result = oc.doa_testbench_create('music_test_input_gen', len_ss, overlap_size, num_ant_ele, FB, 'linear', num_ant_ele, norm_spacing, PERTURB, pilot_doa, nout=3)
+        S_x, S_x_uncalibrated, ant_pert_vec = result
+        S_x_uncalibrated = S_x_uncalibrated.flatten().tolist()
+        ant_pert_vec = list(itertools.chain.from_iterable(ant_pert_vec))
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.vec_source = blocks.vector_source_c(S_x_uncalibrated, False, num_ant_ele*num_ant_ele)
+        self.doa_calibrate_lin_array_0 = calibrate_lin_array(norm_spacing, num_ant_ele, pilot_doa)
+        self.vec_sink = blocks.vector_sink_c(num_ant_ele)        
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.tb.connect((self.vec_source, 0), (self.doa_calibrate_lin_array_0, 0))
+        self.tb.connect((self.doa_calibrate_lin_array_0, 0), (self.vec_sink, 0))  
+
+        # set up fg
+        self.tb.run ()
+
+        # get data from sink
+        ant_pert_vec_est = self.vec_sink.data()
+        ant_pert_vec_est = numpy.asarray(ant_pert_vec_est, 'F')
+        # num of snapshots
+        n_ss = len(ant_pert_vec_est)//num_ant_ele
+
+        # check data
+        ant_ele_range = range(0, num_ant_ele)
+        expected_result = numpy.zeros(num_ant_ele-1)+1j*numpy.zeros(num_ant_ele-1)
+        for ii in range(0, n_ss):
+            # calibrated_antenna_resp will contain elements that are all equal
+            calibrated_antenna_resp = numpy.divide(ant_pert_vec, ant_pert_vec_est[ii*num_ant_ele+numpy.array(ant_ele_range)])
+            self.assertComplexTuplesAlmostEqual(expected_result, numpy.diff(calibrated_antenna_resp), 1)
+ 
 
 if __name__ == '__main__':
     gr_unittest.run(qa_calibrate_lin_array)
--- python/doa/qa_find_local_max.py
+++ python/doa/qa_find_local_max.py
@@ -7,32 +7,107 @@
 #
 
 from gnuradio import gr, gr_unittest
+from gnuradio import blocks
+import oct2py
+import numpy
+import os
+from subprocess import Popen, PIPE
 # from gnuradio import blocks
 try:
     from gnuradio.doa import find_local_max
 except ImportError:
-    import os
     import sys
     dirname, filename = os.path.split(os.path.abspath(__file__))
     sys.path.append(os.path.join(dirname, "bindings"))
     from gnuradio.doa import find_local_max
 
-class qa_find_local_max(gr_unittest.TestCase):
+class qa_find_local_max (gr_unittest.TestCase):
 
-    def setUp(self):
-        self.tb = gr.top_block()
+    def setUp (self):
+        self.tb = gr.top_block ()	
 
-    def tearDown(self):
+    def tearDown (self):
         self.tb = None
 
-    def test_instance(self):
-        # FIXME: Test will fail until you pass sensible arguments to the constructor
-        instance = find_local_max()
+    def test_001_t (self):
+        self.vector_len = 2**11
+        self.num_max_vals = 3
 
-    def test_001_descriptive_test_name(self):
+        # generate vector from octave
+        oc = oct2py.Oct2Py()
+        qa_tests_path = os.path.join(os.path.dirname(__file__), 'qa_tests')
+        oc.addpath(qa_tests_path)
+        # Add the parent directory containing the class folder
+        oc.addpath(os.path.dirname(qa_tests_path))
+        result = oc.test001_findpeaks(self.num_max_vals, self.vector_len, nout=3)
+        data, expected_pks, expected_t_pks = result
+        data = data.flatten().tolist()
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.blocks_vector_source_x_0 = blocks.vector_source_f(data, False, self.vector_len, [])
+        self.doa_find_local_max_0 = find_local_max(self.num_max_vals, self.vector_len, 0.0, 2*numpy.pi)
+        self.blocks_vector_sink_x_0 = blocks.vector_sink_f(self.num_max_vals)
+        self.blocks_vector_sink_x_1 = blocks.vector_sink_f(self.num_max_vals)       
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.tb.connect((self.blocks_vector_source_x_0, 0), (self.doa_find_local_max_0, 0))    
+        self.tb.connect((self.doa_find_local_max_0, 0), (self.blocks_vector_sink_x_0, 0))    
+        self.tb.connect((self.doa_find_local_max_0, 1), (self.blocks_vector_sink_x_1, 0))  
+
         # set up fg
-        self.tb.run()
+        self.tb.run ()
+
+        # get data from sink
+        measured_pks = self.blocks_vector_sink_x_0.data()  
+        measured_pks_locs = self.blocks_vector_sink_x_1.data()  
+
         # check data
+        for i in range(len(measured_pks)):
+            self.assertAlmostEqual(expected_pks[i], measured_pks[i], 5) and self.assertAlmostEqual(expected_t_pks[i], measured_t_pks[i], 5)
+
+    def test_002_t (self):
+        self.vector_len = 2**12
+        self.num_max_vals = 5
+
+        # generate vector from octave
+        oc = oct2py.Oct2Py()
+        qa_tests_path = os.path.join(os.path.dirname(__file__), 'qa_tests')
+        oc.addpath(qa_tests_path)
+        # Add the parent directory containing the class folder
+        oc.addpath(os.path.dirname(qa_tests_path))
+        result = oc.test002_findpeaks(self.num_max_vals, self.vector_len, nout=3)
+        data, expected_pks, expected_t_pks = result
+        data = data.flatten().tolist()
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.blocks_vector_source_x_0 = blocks.vector_source_f(data, False, self.vector_len, [])
+        self.doa_find_local_max_0 = find_local_max(self.num_max_vals, self.vector_len, 0.0, 2*numpy.pi)
+        self.blocks_vector_sink_x_0 = blocks.vector_sink_f(self.num_max_vals)
+        self.blocks_vector_sink_x_1 = blocks.vector_sink_f(self.num_max_vals)       
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.tb.connect((self.blocks_vector_source_x_0, 0), (self.doa_find_local_max_0, 0))    
+        self.tb.connect((self.doa_find_local_max_0, 0), (self.blocks_vector_sink_x_0, 0))    
+        self.tb.connect((self.doa_find_local_max_0, 1), (self.blocks_vector_sink_x_1, 0))  
+
+        # set up fg
+        self.tb.run ()
+
+        # get data from sink
+        measured_pks = self.blocks_vector_sink_x_0.data()  
+        measured_pks_locs = self.blocks_vector_sink_x_1.data()  	
+
+        # check data
+        for i in range(len(measured_pks)):
+            self.assertAlmostEqual(expected_pks[i], measured_pks[i], 5) and self.assertAlmostEqual(expected_t_pks[i], measured_t_pks[i], 5)
 
 
 if __name__ == '__main__':
